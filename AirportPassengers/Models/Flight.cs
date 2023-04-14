38b5034be52a4bfcc767e2d8f034b68831e62e05
Artur Sizov <artur-sa@mail.ru>
2023-04-14 11:07:45 +0300

    Добавьте файлы проекта.

---
+++ AirportPassengers/Models/Flight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirportPassengers.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public int Number { get; set;}
+        public DateTime DepartureTime { get; set; }
+        public IEnumerable<Passenger> Passengers { get; set; }
+    }
+}
---
+++ AirportPassengers/Models/Passenger.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportPassengers.Models
+{
+    public class Passenger
+    {
+        public int IdFlight { get; set; }       
+        public int IdPassenger { get; set; }
+        public string Name { get; set; }    
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
